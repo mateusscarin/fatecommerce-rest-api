3bd5b3b52d0341dc096e5dd6476a715bc7ac44ff
mateusscarin <mateus.scarin@gmail.com>
2024-08-10 15:12:53 -0300

    feat: initialize project

---
+++ src/main/java/br/com/fatecommerce/api/FatecommerceRestApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatecommerce.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatecommerceRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FatecommerceRestApiApplication.class, args);
+	}
+
+}
