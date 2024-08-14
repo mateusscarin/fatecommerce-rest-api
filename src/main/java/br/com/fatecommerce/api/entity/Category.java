ea283d0c7ae8c58e70e8323f30e4450f101b039e
mateusscarin <mateus.scarin@gmail.com>
2024-08-14 20:36:15 -0300

    feat: entidade Category

---
+++ src/main/java/br/com/fatecommerce/api/entity/Category.java
@@ -0,0 +1,33 @@
+package br.com.fatecommerce.api.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column(nullable = false, length = 300, unique = true)
+    private String name;
+
+    @Column(length = 1000)
+    private String description;
+
+}
